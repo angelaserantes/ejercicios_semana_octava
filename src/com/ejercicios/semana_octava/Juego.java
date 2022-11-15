2143de35774ab1ec54e9030e4c7a46248d00b7d1
angelaserantes <aserantesreboreda@danielcastelao.org>
2022-11-15 17:42:37 +0100

    Ejercicio5

---
+++ src/com/ejercicios/semana_octava/Juego.java
@@ -0,0 +1,32 @@
+package com.ejercicios.semana_octava;
+
+import java.util.Scanner;
+
+public class Juego {
+    public static void ComprobarNumeros() {
+        int n1 = (int) (Math.random() * 10) + 1;
+        System.out.println("El número está entre el 1 y el 10 ");
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Dime el número");
+        for (int i = 0; i <=n1; i++){
+            int n2 = entrada.nextInt();
+            if (n1 == n2) {
+                System.out.println("Acertaste");
+                break;
+            } else if (n1 > n2) {
+                System.out.println("El número es mayor");
+            } else {
+                System.out.println("El número es menor");
+            }
+    }
+
+
+    }
+
+    public static void main(String[] args) {
+        ComprobarNumeros();
+    }
+}
+
+
+
